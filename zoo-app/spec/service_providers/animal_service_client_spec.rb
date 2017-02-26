634333dc1404867978b1fbc117be5949dcd3e50f
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2017-02-26 21:13:17 +0900

    I Write a failing spec for the Animal Service client

---
+++ zoo-app/spec/service_providers/animal_service_client_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../../lib/zoo_app/animal_service_client'
+require_relative 'pact_helper'
+require_relative '../../lib/zoo_app/models/alligator'
+
+describe AnimalServiceClient, :pact => true do
+
+  before do
+    # Configure your client to point to the stub service on localhost using the port you have specified
+    AnimalServiceClient.base_uri 'localhost:1234'
+  end
+
+  subject { AnimalServiceClient.new }
+
+  describe "get_alligator" do
+
+    before do
+      animal_service.given("an alligator exists").
+        upon_receiving("a request for an alligator").
+        with(method: :get, path: '/alligator', query: '').
+        will_respond_with(
+          status: 200,
+          headers: {'Content-Type' => 'application/json'},
+          body: {name: 'Betty'} )
+    end
+
+    it "returns a alligator" do
+      expect(subject.get_alligator).to eq(Alligator.new('Betty'))
+    end
+
+  end
+
+end
